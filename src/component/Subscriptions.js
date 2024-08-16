97289976aaf83a52b4108f72370bdbe7d4eaca21
Been980804 <dlgusqls980804@naver.com>
2024-08-16 14:53:33 +0900

    feat: table css 통일

--- src/component/Subscriptions.js
+++ src/component/Subscriptions.js
@@ -51,23 +51,23 @@ const Subscriptions = () => {
   return (
     <div className="container mt-5 py-4 px-xl-5">
       <h3 className="text-2xl font-bold mt-5 mb-4">구독 리스트</h3>
-      <table className="min-w-full bg-white border border-gray-300">
-        <thead>
-          <tr className="bg-gray-100">
-            <th className="py-2 px-4 border-b">이미지</th>
-            <th className="py-2 px-4 border-b">상품명</th>
-            <th className="py-2 px-4 border-b">구독일</th>
-            <th className="py-2 px-4 border-b">구독주기</th>
-            <th className="py-2 px-4 border-b">배송출발예정일</th>
-            <th className="py-2 px-4 border-b">구독횟수</th>
-            <th className="py-2 px-4 border-b">상태</th>
-            <th className="py-2 px-4 border-b">삭제</th>
+      <table className="common-table min-w-full bg-white border border-gray-300">
+        <thead className="common-thead">
+          <tr className="common-tr bg-gray-100">
+            <th className="common-th py-2 px-4 border-b">이미지</th>
+            <th className="common-th py-2 px-4 border-b">상품명</th>
+            <th className="common-th py-2 px-4 border-b">구독일</th>
+            <th className="common-th py-2 px-4 border-b">구독주기</th>
+            <th className="common-th py-2 px-4 border-b">배송출발예정일</th>
+            <th className="common-th py-2 px-4 border-b">구독횟수</th>
+            <th className="common-th py-2 px-4 border-b">상태</th>
+            <th className="common-th py-2 px-4 border-b">삭제</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="common-tbody">
           {subscriptions.map((subscription) => (
-            <tr key={subscription.subNum}>
-              <td className="py-2 px-4 border-b">
+            <tr key={subscription.subNum} className="common-tr">
+              <td className="common-td py-2 px-4 border-b">
                 <img
                   src={subscription.imgURL}
                   width="80"
@@ -75,7 +75,7 @@ const Subscriptions = () => {
                   alt="상품 이미지"
                 />
               </td>
-              <td className="py-2 px-4 border-b">
+              <td className="common-td py-2 px-4 border-b">
                 <Link
                   className="color-green"
                   to={`/products/${subscription.prodNum}`}
@@ -83,19 +83,19 @@ const Subscriptions = () => {
                   {subscription.prodName}
                 </Link>
               </td>
-              <td className="py-2 px-4 border-b">
+              <td className="common-td py-2 px-4 border-b">
                 {subscription.subStart.split("T")[0]}
               </td>
-              <td className="py-2 px-4 border-b">
+              <td className="common-td py-2 px-4 border-b">
                 {subscription.subPer + "일"}
               </td>
-              <td className="py-2 px-4 border-b">
+              <td className="common-td py-2 px-4 border-b">
                 {subscription.subDeli.split("T")[0]}
               </td>
-              <td className="py-2 px-4 border-b">
+              <td className="common-td py-2 px-4 border-b">
                 {subscription.subCnt + "회"}
               </td>
-              <td className="py-2 px-4 border-b">
+              <td className="common-td py-2 px-4 border-b">
                 {subscription.subStat === 1
                   ? "배송중"
                   : subscription.subStat === 2
@@ -104,10 +104,10 @@ const Subscriptions = () => {
                   ? "대기중"
                   : "알 수 없음"}
               </td>
-              <td className="py-2 px-4 border-b">
+              <td className="common-td py-2 px-4 border-b">
                 <button
                   onClick={() => handleDelete(subscription.subNum)}
-                  className="delete-button"
+                  className="common-button"
                 >
                   삭제
                 </button>
