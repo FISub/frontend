fc36e5b418657e802c8ec388e9b9b520e80c6559
leesj000603 <leesj000603@daum.net>
2024-08-16 10:01:21 +0900

    feat : subscriptions 페이지 레이아웃 수정

--- src/component/Subscriptions.js
+++ src/component/Subscriptions.js
@@ -28,28 +28,40 @@ const Subscriptions = () => {
   if (error) return <div>Error fetching subscriptions.</div>;
 
   return (
-    <div className="subscriptions-container">
-      {subscriptions.length === 0 ? (
-        <div>No subscriptions found.</div>
-      ) : (
-        <div className="subscriptions-list">
-          {subscriptions.map(sub => (
-            <div className="subscription-card" key={sub.subNum}>
-              <img src={sub.imgURL} alt={sub.prodNum} className="subscription-image" />
-              <div className="subscription-info">
-                <h3>구독번호: {sub.subNum}</h3>
-                <p>구독 시작일: {new Date(sub.subStart).toLocaleDateString()}</p>
-                <p>구독 배송일: {new Date(sub.subDeli).toLocaleDateString()}</p>
-                <p>구독 기간: {sub.subPer}개월</p>
-                <p>상품 번호: {sub.prodNum}</p>
-                <p>결제 수단: {sub.payNum}</p>
-              </div>
-            </div>
+    <div className="container mt-5 py-4 px-xl-5">
+      <h3 className="text-2xl font-bold mt-5 mb-4">구독 리스트</h3>
+      <table className="min-w-full bg-white border border-gray-300">
+        <thead>
+          <tr className="bg-gray-100">
+            <th className="py-2 px-4 border-b">이미지</th>
+            <th className="py-2 px-4 border-b">상품명</th>
+            <th className="py-2 px-4 border-b">구독일</th>
+            <th className="py-2 px-4 border-b">구독주기</th>
+            <th className="py-2 px-4 border-b">배송출발예정일</th>
+            <th className="py-2 px-4 border-b">구독횟수</th>     
+            <th className="py-2 px-4 border-b">상태</th>            
+          </tr>
+        </thead>
+        <tbody>
+          {subscriptions.map((subscriptions) => (
+            <tr key={subscriptions.sub_num}>
+              <td className="py-2 px-4 border-b"><img src={subscriptions.imgURL} width="80" height="80" /></td>
+              <td className="py-2 px-4 border-b">{subscriptions.prodName}</td>
+              <td className="py-2 px-4 border-b">{subscriptions.subStart.split('T')[0]}</td>
+              <td className="py-2 px-4 border-b">{subscriptions.subPer + "일"}</td>
+              <td className="py-2 px-4 border-b">{subscriptions.subDeli.split('T')[0]}</td>
+              <td className="py-2 px-4 border-b">{subscriptions.subCnt + "회"}</td>
+              <td className="py-2 px-4 border-b">{subscriptions.subStat === 1 ? "배송중" 
+                : subscriptions.subStat === 2 ? "배송완료" 
+                : subscriptions.subStat === 3 ? "대기중" 
+                : "알 수 없음"}
+                </td>
+            </tr>
           ))}
-        </div>
-      )}
+        </tbody>
+      </table>
     </div>
   );
-};
+}
 
 export default Subscriptions;
