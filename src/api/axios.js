88177543f0bbb01cf24698493149258d2c09dc91
leesj <leesj000603@daum.net>
2024-09-18 21:36:19 +0900

    feat : https전환, spring security전환에 따른 proxy 설정 포트 8080 -> 8443수정

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://localhost:8080",
+  baseURL: "https://localhost:8443",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
